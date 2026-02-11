7dfc34bd960d83a1b1e865ff4498e7fbe7d362c3
EliaseOsmani <eliaseosmani@icloud.com>
2026-02-10 21:07:21 -0800

    Initial commit

---
+++ RetuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetuneApp.swift
+//  Retune
+//
+//  Created by Eliase Osmani on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct RetuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
